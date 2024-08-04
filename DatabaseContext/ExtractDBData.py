fda140acf831c306e72c531c12718a8e4fd4cd33
charlesnolte <charles.nolte@flash.co.za>
2024-08-04 22:22:27 +0200

    Update province-quarter

--- DatabaseContext/ExtractDBData.py
+++ DatabaseContext/ExtractDBData.py
@@ -27,47 +27,6 @@ def connect_to_database():
     cnxn = pyodbc.connect(f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}')
     return cnxn
 
-def fetch_crime_stats(provincecode, policestation, year=None, quarter=None):
-    # Connect to the database
-    cnxn = connect_to_database()
-    cursor = cnxn.cursor()
-
-    try:
-        # Execute the stored procedure with the parameters
-        if year:  # Check if the year parameter is not empty
-            cursor.execute("EXEC sp_GetQuartilyCrimeStatsPerQuarter ?, ?, ?, ?", (provincecode.strip(), policestation.strip(), int(year), int(quarter)))
-        else:
-            cursor.execute("EXEC sp_GetQuartilyCrimeStatsPerQuarter ?, ?, ?, ?", (provincecode.strip(), policestation.strip(), None, int(quarter)))
-
-        # Fetch the results after executing the stored procedure
-        rows = cursor.fetchall()
-
-        # Fetch the column descriptions (headings)
-        headings = [column[0] for column in cursor.description]
-
-        # Reshape the rows data to match the expected shape
-        rows = [list(row) for row in rows]
-
-        # Create a DataFrame from the fetched rows and headings
-        df_stats = pd.DataFrame(rows, columns=headings)
-
-        # Replace null values with 0's
-        df_stats.fillna(0, inplace=True)
-
-        print(df_stats)
-
-        return df_stats  # Return the DataFrame
-
-    except pyodbc.Error as e:
-        # Print an error message if there's an exception
-        print("Error executing SQL query:", e)
-        return None  # Return None if there's an error
-
-    finally:
-        # Close the cursor and connection
-        cursor.close()
-        cnxn.close()
-
 
 def fetch_all_provinces():
     # Connect to the database
@@ -104,7 +63,6 @@ def fetch_all_provinces():
         cursor.close()
         cnxn.close()
 
-
 def fetch_policestation_per_provinces(provincecode):
     # Connect to the database
     cnxn = connect_to_database()
@@ -139,3 +97,157 @@ def fetch_policestation_per_provinces(provincecode):
         # Close the cursor and connection
         cursor.close()
         cnxn.close()    
+
+def fetch_prediction_province_policestation():
+    # Connect to the database
+    cnxn = connect_to_database()
+    cursor = cnxn.cursor()
+
+    try:   
+        cursor.execute("EXEC sp_Get_Prediction_Province_PoliceStation")
+
+        # Fetch the results after executing the stored procedure
+        rows = cursor.fetchall()
+        print(rows)
+
+        # Fetch the column descriptions (headings)
+        headings = [column[0] for column in cursor.description]
+
+        # Reshape the rows data to match the expected shape
+        rows = [list(row) for row in rows]
+
+        # Create a DataFrame from the fetched rows and headings
+        df = pd.DataFrame(rows, columns=headings)
+
+        # Replace null values with 0's
+        df.fillna(0, inplace=True)
+
+        return df  # Return the DataFrame
+
+    except pyodbc.Error as e:
+        # Print an error message if there's an exception
+        print("Error executing SQL query:", e)
+        return None  # Return None if there's an error
+
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+        cnxn.close()
+
+def read_stats_province_policestation(provincecode, policestationcode):
+    # Connect to the database
+    cnxn = connect_to_database()
+    cursor = cnxn.cursor()
+
+    try:
+        # Execute the stored procedure with the parameters
+        cursor.execute("EXEC sp_Get_Stats_Province_PoliceStation ?, ?", (provincecode.strip(), policestationcode.strip())) 
+
+        # Fetch the results after executing the stored procedure
+        rows = cursor.fetchall()
+
+        # Fetch the column descriptions (headings)
+        headings = [column[0] for column in cursor.description]
+
+        # Reshape the rows data to match the expected shape
+        rows = [list(row) for row in rows]
+
+        # Create a DataFrame from the fetched rows and headings
+        df_stats = pd.DataFrame(rows, columns=headings)
+
+        # Replace null values with 0's
+        df_stats.fillna(0, inplace=True)
+
+        return df_stats  # Return the DataFrame
+
+    except pyodbc.Error as e:
+        # Print an error message if there's an exception
+        print("Error executing SQL query:", e)
+        return None  # Return None if there's an error
+
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+        cnxn.close()
+
+def read_stats_province_policestation_quarterly(provincecode, policestationcode, quarter=None):
+    # Connect to the database
+    cnxn = connect_to_database()
+    cursor = cnxn.cursor()
+
+    try:
+        # Execute the stored procedure with the parameters
+        # if year:  # Check if the year parameter is not empty
+        #     cursor.execute("EXEC sp_GetQuartilyCrimeStatsPerQuarter ?, ?, ?, ?", (provincecode.strip(), policestation.strip(), int(year), int(quarter)))
+        # else:
+        #     cursor.execute("EXEC sp_GetQuartilyCrimeStatsPerQuarter ?, ?, ?, ?", (provincecode.strip(), policestation.strip(), None, int(quarter)))
+
+
+        cursor.execute("EXEC sp_Get_Stats_Province_PoliceStation_Quarterly ?, ?, ?", (provincecode.strip(), policestationcode.strip(), int(quarter)))
+
+        # Fetch the results after executing the stored procedure
+        rows = cursor.fetchall()
+
+        # Fetch the column descriptions (headings)
+        headings = [column[0] for column in cursor.description]
+
+        # Reshape the rows data to match the expected shape
+        rows = [list(row) for row in rows]
+
+        # Create a DataFrame from the fetched rows and headings
+        df_stats = pd.DataFrame(rows, columns=headings)
+
+        # Replace null values with 0's
+        df_stats.fillna(0, inplace=True)
+
+        return df_stats  # Return the DataFrame
+
+    except pyodbc.Error as e:
+        # Print an error message if there's an exception
+        print("Error executing SQL query:", e)
+        return None  # Return None if there's an error
+
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+        cnxn.close()
+
+def read_stats_province_quarterly(provincecode, quarter=None):
+    # Connect to the database
+    cnxn = connect_to_database()
+    cursor = cnxn.cursor()
+
+    try:
+        print("Province: " ,provincecode)
+        print("Quarter : " ,quarter)
+
+        cursor.execute("EXEC sp_Get_Stats_Province_Quarterly ?, ?", (provincecode.strip(), int(quarter)))
+
+        # Fetch the results after executing the stored procedure
+        rows = cursor.fetchall()
+
+        # Fetch the column descriptions (headings)
+        headings = [column[0] for column in cursor.description]
+
+        # Reshape the rows data to match the expected shape
+        rows = [list(row) for row in rows]
+
+        # Create a DataFrame from the fetched rows and headings
+        df_stats = pd.DataFrame(rows, columns=headings)
+
+        # Replace null values with 0's
+        df_stats.fillna(0, inplace=True)
+
+        print(df_stats)
+
+        return df_stats  # Return the DataFrame
+
+    except pyodbc.Error as e:
+        # Print an error message if there's an exception
+        print("Error executing SQL query:", e)
+        return None  # Return None if there's an error
+
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+        cnxn.close()
--- main.py
+++ main.py
@@ -5,22 +5,20 @@ from DatabaseContext import ExtractDBData
 
 app = FastAPI()
 
-@app.get("/items_crime_stats/")
-def read_crime_data(provincecode: str, policestation: str, year: Optional[int] = None, quarter: Optional[int] = None):
-    
-    # Call function from ExtractDBData.py to fetch data based on parameters
-    fetch_crime_data = ExtractDBData.fetch_crime_stats(provincecode, policestation, year, quarter)
 
-    if fetch_crime_data is None:
-        return {"error": "Crime data not found"}
+@app.get("/fetch_prediction_province_policestation/")
+def read_provinces01_data():
+
+    fetch_prediction_province_policestation_data = ExtractDBData.fetch_prediction_province_policestation()
+
+    if fetch_prediction_province_policestation_data is None:
+        return {"error": "Province data not found"}
     
     # Convert DataFrame to JSON serializable format (list of dictionaries)
-    return fetch_crime_data.to_dict(orient='records')
+    return fetch_prediction_province_policestation_data.to_dict(orient='records')
 
-@app.get("/items_fetch_all_provinces/")
+@app.get("/fetch_all_provinces/")
 def read_provinces_data():
-
-    # Call function from ExtractDBData.py to fetch data based on parameters
 
     fetch_province_data = ExtractDBData.fetch_all_provinces()
 
@@ -30,7 +28,7 @@ def read_provinces_data():
     # Convert DataFrame to JSON serializable format (list of dictionaries)
     return fetch_province_data.to_dict(orient='records')
 
-@app.get("/items_policestation_per_provinces/")
+@app.get("/fetch_policestation_per_provinces/")
 def read_policestation_data(provincecode : str ):
 
     #= Query(..., description="Province code")
@@ -42,3 +40,99 @@ def read_policestation_data(provincecode : str ):
     
     # Convert DataFrame to JSON serializable format (list of dictionaries)
     return fetch_poilcestation_data.to_dict(orient='records')
+
+@app.get("/fetch_stats_province_policestation/")
+def read_stats_province_policestation(provincecode: str, policestationcode: str):
+
+    # Call function from ExtractDBData.py to fetch data based on parameters
+    fetch_crime_data = ExtractDBData.read_stats_province_policestation(provincecode, policestationcode)
+
+    if fetch_crime_data is None:
+        return {"error": "Crime data not found"}
+    
+    # Convert DataFrame to JSON serializable format (list of dictionaries)
+    print('Final Dataset : ')
+    return fetch_crime_data.to_dict(orient='records')
+
+@app.get("/fetch_stats_province_policestation_quarterly/")
+def read_stats_province_policestation_quarterly(provincecode: str, policestationcode: str, quarter: Optional[int] = None):
+
+    # Call function from ExtractDBData.py to fetch data based on parameters
+    fetch_crime_data = ExtractDBData.read_stats_province_policestation_quarterly(provincecode, policestationcode, quarter)
+
+    if fetch_crime_data is None:
+        return {"error": "Crime data not found"}
+    
+    # Convert DataFrame to JSON serializable format (list of dictionaries)
+    return fetch_crime_data.to_dict(orient='records')
+
+@app.get("/fetch_stats_province_year_quarterly/")
+def read_stats_province_quarterly(provincecode : str, quarter: Optional[int] = None ):
+   
+    print(provincecode)
+    print(quarter)
+    
+    # Call function from ExtractDBData.py to fetch data based on parameters
+    fetch_crime_data = ExtractDBData.read_stats_province_quarterly(provincecode, quarter)
+
+    if fetch_crime_data is None:
+        return {"error": "Crime data not found"}
+    
+    # Convert DataFrame to JSON serializable format (list of dictionaries)
+    return fetch_crime_data.to_dict(orient='records')
+
+
+@app.post("/fetch_predition_province_policestation_year_quarterly/")
+def read_predition_province_policestation_year_quarterly():
+   
+       # Call function from ExtractDBData.py to fetch data based on parameters
+    
+    fetch_prediction_province_policestation = ExtractDBData.fetch_prediction_province_policestation_data()
+
+    if fetch_prediction_province_policestation is None:
+        return {"error": "Province data not found"}
+    
+    # Convert DataFrame to JSON serializable format (list of dictionaries)
+    return fetch_prediction_province_policestation.to_dict(orient='records')
+
+@app.post("/fetch_best_predictions_province_policestation_year_quarterly/")
+def read_best_predictions_province_policestation_year_quarterly(provincecode : str ):
+   
+    # Convert DataFrame to JSON serializable format (list of dictionaries)
+    return 0
+
+@app.post("/add_prediction_knn_province_policestation_year_quarterly/")
+def create_prediction_knn_province_policestation_year_quarterly(provincecode : str ):
+   
+    # Convert DataFrame to JSON serializable format (list of dictionaries)
+    return 0
+
+@app.post("/add_prediction_mlpregressor_province_policestation_year_quarterly/")
+def create_prediction_mlpregressor_province_policestation_year_quarterly(provincecode : str ):
+   
+    # Convert DataFrame to JSON serializable format (list of dictionaries)
+    return 0
+
+@app.post("/add_prediction_svr_province_policestation_year_quarterly/")
+def create_prediction_svr_province_policestation_year_quarterly(provincecode : str ):
+   
+    # Convert DataFrame to JSON serializable format (list of dictionaries)
+    return 0
+
+@app.post("/add_prediction_rfm_province_policestation_year_quarterly/")
+def create_prediction_rfm_province_policestation_year_quarterly(provincecode : str ):
+   
+    # Convert DataFrame to JSON serializable format (list of dictionaries)
+    return 0
+
+@app.post("/add_prediction_xgboost_province_policestation_year_quarterly/")
+def create_prediction_xgboost_province_policestation_year_quarterly(provincecode : str ):
+   
+    # Convert DataFrame to JSON serializable format (list of dictionaries)
+    return 0
+
+@app.post("/update_best_predictions/")
+def update_best_prediction(provincecode : str ):
+   # will set a flag for the best 2 priction AcceptPredict: 1 = true , 0 = false
+    # Convert DataFrame to JSON serializable format (list of dictionaries)
+    return 0
